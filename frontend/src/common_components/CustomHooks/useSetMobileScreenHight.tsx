bf42a0c3b19e021cc44adc08d6d9b051fbcbe035
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-09-10 15:15:25 -0700

    Added comment for useSetMobileScreenHight hook

--- frontend/src/common_components/CustomHooks/useSetMobileScreenHight.tsx
+++ frontend/src/common_components/CustomHooks/useSetMobileScreenHight.tsx
@@ -1,8 +1,11 @@
 import { useCallback, useEffect } from "react";
 import useDetectMobile from "./useDetectMobile";
 
+// Mobile screens have different innerHeights, so adjust screen size for them.
 const useSetMobileScreenHight = () => {
     const isMobile = useDetectMobile();
+
+    // Get innerHeight which is actual visible screen area size.
     const appHeight = useCallback(() => {
         if (!isMobile) return;
         const doc = document.documentElement;
@@ -13,6 +16,7 @@ const useSetMobileScreenHight = () => {
         const doc = document.documentElement;
         const currAppHeight = doc.style.getPropertyValue('--app-height');
 
+        // If there's no --app-height set yet, call appHeight to set initial css value. 
         if (currAppHeight === '') {
             appHeight();
         }
